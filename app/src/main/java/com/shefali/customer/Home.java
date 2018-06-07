0ccd49560a81476cd09697fd6d930945bfcf43a7
shefali.msr <201551095@iiitvadodara.ac.in>
2018-06-08 00:15:37 +0530

    second

--- app/src/main/java/com/shefali/customer/Home.java
+++ app/src/main/java/com/shefali/customer/Home.java
@@ -1,5 +1,6 @@
 package com.shefali.customer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -8,6 +9,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import static android.graphics.Color.rgb;
 
@@ -15,6 +18,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
     private DrawerLayout mDrawerLayout;
     private Toolbar mToolbar;
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,18 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        start = (Button) findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+
+                Intent myIntent = new Intent(Home.this,
+                        ChooseLocation.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
     @Override
